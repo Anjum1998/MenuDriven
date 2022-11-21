e2f81c22b782fa7d927e72b5f8aa23180f7c8119
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-21 12:58:09 +0530

    adding prime

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -49,6 +49,29 @@ public class MenuDriven {
                     System.out.println(g + "is smallest");
                 }
                 break;
+            case 3:
+                System.out.println("Enter the Number");
+                int num= sc.nextInt();
+                boolean flag=false;
+                for(int i=2;i<=num/2;i++)
+                {
+                    if(num%i==0)
+                    {
+                        flag =true;
+                        break;
+                    }
+                }
+                if(!flag)
+                {
+                    System.out.println(num+ " is prime number");
+                }
+                else
+                {
+                    System.out.println(num + "is not prime number");
+                }
+                break;
+
+
 
         }
 
